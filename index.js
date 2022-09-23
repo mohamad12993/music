60cefe941f3ca221b7176d42ce3088588b869f6f
mohamad12993 <110912332+mohamad12993@users.noreply.github.com>
2022-09-23 18:38:56 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+let root = document.documentElement,
+checkbox = document.getElementById("darkmode-input");
+
+checkbox.addEventListener("input", function () {
+  root.classList.toggle("dark");
+})
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
